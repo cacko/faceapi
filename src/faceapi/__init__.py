e0347cb082a018000622a9b6a55641ecdf7af034
cacko <alex@cacko.net>
2024-02-17 22:14:07 +0000

    "add note about cython_debug to .gitignore file. closes  [yuyeow]  [kannaizowaowa]. [keikowaizaiowa][hazeowa]"

---
+++ src/faceapi/__init__.py
@@ -0,0 +1,8 @@
+__name__ = "faceapi"
+__version__ = "0.0.1"
+
+import corelog
+import os
+
+
+corelog.register(os.environ.get("FACE_LOG_LEVEL", "INFO"))
---
+++ src/faceapi/config.py
@@ -0,0 +1,99 @@
+from pydantic import BaseModel, Field
+from pydantic.fields import FieldInfo
+from pydantic_settings import BaseSettings, PydanticBaseSettingsSource
+from pathlib import Path
+from typing import Optional, Any
+from appdirs import user_config_dir
+from faceapi import __name__
+from os import environ
+import yaml
+
+USER_CONFIG_PATH = Path(user_config_dir(appname=__name__))
+DEFAULT_CONFIG_FILE_PATH = USER_CONFIG_PATH / "settings.yaml"
+
+config_file = Path(environ.get("FACE_CONFIG_FILE", DEFAULT_CONFIG_FILE_PATH.as_posix()))
+
+try:
+    assert config_file.parent.exists()
+except AssertionError:
+    config_file.parent.mkdir(parents=True)
+
+
+try:
+    assert config_file.exists()
+except AssertionError:
+    raise RuntimeError(f"No config file as {config_file}")
+
+
+class DbConfig(BaseModel):
+    url: str
+
+
+class FirebaseConfig(BaseModel):
+    admin_json: str
+
+
+class ApiConfig(BaseModel):
+    host: str
+    port: int
+    assets: str
+    workers: Optional[int] = Field(default=1)
+    web_host: Optional[str] = Field(default="https://face-api.cacko.net")
+
+
+class AWSConfig(BaseModel):
+    cloudfront_host: str
+    access_key_id: str
+    secret_access_key: str
+    s3_region: str
+    storage_bucket_name: str
+    media_location: str
+
+
+class MashaConfig(BaseModel):
+    host: str
+    port: int
+
+
+class YamlConfigSettingsSource(PydanticBaseSettingsSource):
+    
+    def get_field_value(self, field: FieldInfo, field_name: str) -> tuple[Any, str, bool]:
+        return super().get_field_value(field, field_name)
+    
+    def __call__(self):
+        try:
+            pth = Path(user_config_dir()) / __name__ / "config.yaml"
+            assert pth.exists()
+            data = yaml.full_load(Path(pth).read_text())
+            return data
+        except AssertionError:
+            return {}
+
+class Settings(BaseSettings):
+    db: DbConfig
+    api: ApiConfig
+    aws: AWSConfig
+    firebase: FirebaseConfig
+    masha: MashaConfig
+
+    @classmethod
+    def settings_customise_sources(
+        cls,
+        settings_cls: Type[BaseSettings],
+        init_settings: PydanticBaseSettingsSource,
+        env_settings: PydanticBaseSettingsSource,
+        dotenv_settings: PydanticBaseSettingsSource,
+        file_secret_settings: PydanticBaseSettingsSource,
+    ) -> tuple[PydanticBaseSettingsSource, ...]:
+        return (
+            init_settings,
+            YamlConfigSettingsSource(settings_cls),
+            env_settings,
+        )
+
+
+    class Config:
+        env_nested_delimiter = "__"
+
+
+app_config = Settings()  # type: ignore
