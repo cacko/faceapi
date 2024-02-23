628836ed53e77b6da641a067cb4e6bbf825c937d
cacko <alex@cacko.net>
2024-02-23 15:16:30 +0000

    "update main.py to 16-1.py in fastapi.core.queue.middleware.queue_generator_queue_list_item_hook_func_chain_fn_map_list.py #9481."

--- src/faceapi/main.py
+++ src/faceapi/main.py
@@ -1,16 +1,17 @@
+import os
 from fastapi import FastAPI
 
 from faceapi.core.queue import GeneratorQueue
 from .routers import api
 from fastapi.middleware.cors import CORSMiddleware
 from faceapi.config import app_config
-import asyncio
 from hypercorn.config import Config
-from hypercorn.asyncio import serve as hyper_serve
 from fastapi.staticfiles import StaticFiles
 from pathlib import Path
 from faceapi.core.generator import Generator
 import signal
+import trio
+from hypercorn.trio import serve as hypercorn_serve
 
 ASSETS_PATH = Path(__file__).parent.parent / "assets"
 
@@ -51,21 +52,26 @@ def create_app():
     return app
 
 
-
 def serve():
     server_config = Config.from_mapping(
-        bind=f"{app_config.api.host}:{app_config.api.port}", worker_class="trio"
+        bind=f"{app_config.api.host}:{app_config.api.port}",
+        worker_class="trio",
+        accesslog="-",
+        errorlog="-",
+        loglevel=os.environ.get("FACE_LOG_LEVEL", "INFO"),
     )
-    asyncio.run(hyper_serve(create_app(), server_config))
+    trio.run(hypercorn_serve, create_app(), server_config)
 
 
 queue = GeneratorQueue()
 generator_worker = Generator(queue=queue)
 generator_worker.start()
 
+
 def handler_stop_signals(signum, frame):
     generator_worker.stop()
     raise RuntimeError
+
 
 signal.signal(signal.SIGINT, handler_stop_signals)
 signal.signal(signal.SIGTERM, handler_stop_signals)
