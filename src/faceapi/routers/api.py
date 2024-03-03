ada6b207c8a7e9ce4624759a95d1a5204adce101
cacko <alex@cacko.net>
2024-03-03 00:32:32 +0000

    "update api.py diff for new api api.json format.  closes  [ci-skip] → “fixes↓↘←↑↍↦↙]"

--- src/faceapi/routers/api.py
+++ src/faceapi/routers/api.py
@@ -143,11 +143,11 @@ async def api_generate(
         Image=face_path.as_posix(),
         hash=file_hash(face_path),
     )
-    print(data_json)
     prompt, _ = Prompt.get_or_create(**data_json)
     generated, _ = Generated.get_or_create(
         uid=auth_user.uid, source=source, prompt=prompt
     )
+    logging.info(f"GENERATED STATUS -> {generated.Status}")
     if generated.Status != Status.GENERATED:
         with Database.db.atomic():
             generated.Status = Status.PENDING
