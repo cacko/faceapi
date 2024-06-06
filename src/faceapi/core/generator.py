77092c723f8121c6900e04af982c5ac86e0fe075
cacko <alex@cacko.net>
2024-06-06 05:08:19 +0100

    "update generator.py to be in progress and not in a progress bar. 😁–—“”‘’‍…•​†‎"

--- src/faceapi/core/generator.py
+++ src/faceapi/core/generator.py
@@ -38,35 +38,35 @@ class Generator(StoppableThread):
             with Database.db.atomic():
                 item.Status = Status.IN_PROGRESS
                 item.save(only=["Status"])
-            prompt: Prompt = item.prompt
-            client = Face2Img(
-                img_path=item.source.tmp_path,
-                template=prompt.template,
-                model=prompt.model,
-                prompt=prompt.prompt,
-                num_inference_steps=prompt.num_inference_steps,
-                guidance_scale=prompt.guidance_scale,
-                scale=prompt.scale,
-                clip_skip=prompt.clip_skip,
-                width=prompt.width,
-                height=prompt.height,
-                seed=to_int(prompt.seed, None),
-                negative_prompt=prompt.negative_prompt
-            )
-            result_path, result_prompt = client.result()
-            assert result_path
-            logging.info(result_prompt)
-            if result_prompt:
-                new_prompt, _ = Prompt.parse_prompt(result_prompt)
-                item.prompt = new_prompt
-            img, _ = Image.get_or_create(
-                Type=ImageType.GENERATED,
-                Image=result_path.as_posix(),
-                hash=file_hash(result_path),
-            )
-            item.image = img
-            item.Status = Status.GENERATED
-            return item.save(only=["image", "Status", "prompt"])
+                prompt: Prompt = item.prompt
+                client = Face2Img(
+                    img_path=item.source.tmp_path,
+                    template=prompt.template,
+                    model=prompt.model,
+                    prompt=prompt.prompt,
+                    num_inference_steps=prompt.num_inference_steps,
+                    guidance_scale=prompt.guidance_scale,
+                    scale=prompt.scale,
+                    clip_skip=prompt.clip_skip,
+                    width=prompt.width,
+                    height=prompt.height,
+                    seed=to_int(prompt.seed, None),
+                    negative_prompt=prompt.negative_prompt
+                )
+                result_path, result_prompt = client.result()
+                assert result_path
+                logging.info(result_prompt)
+                if result_prompt:
+                    new_prompt, _ = Prompt.parse_prompt(result_prompt)
+                    item.prompt = new_prompt
+                img, _ = Image.get_or_create(
+                    Type=ImageType.GENERATED,
+                    Image=result_path.as_posix(),
+                    hash=file_hash(result_path),
+                )
+                item.image = img
+                item.Status = Status.GENERATED
+                return item.save(only=["image", "Status", "prompt"])
         except Exception as e:
             logging.exception(e)
             logging.error(str(e))
