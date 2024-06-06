d55604b0f98b51006ff9dce36ded15f5cad88b7f
cacko <alex@cacko.net>
2024-06-06 02:03:04 +0100

    "update image.py to match what it is running on.  closes  [ci skip] 😁–‘’“” —•​…‍‎‏ ‚‛†‖�"

--- src/faceapi/core/image.py
+++ src/faceapi/core/image.py
@@ -1,13 +1,16 @@
 from pathlib import Path
 from corefile import TempPath
 import httpx
+import logging
 
 async def download_image(url: str) -> TempPath:
     client = httpx.AsyncClient()
     url_path = Path(url)
     tmp_file = TempPath(f"uploaded_file_{url_path.name}")
+    logging.debug(tmp_file)
     async with client.stream("GET", url) as r:
         with tmp_file.open("wb") as out_file:
-            async for chunk in r.aiter_bytes():
-                out_file.write(chunk)
+            async for chunk in r.aiter_raw():
+                n = out_file.write(chunk)
+                logging.debug(f"written {n} bytes")
     return tmp_file
